6b3b57f0ecd76d237b7789eaa1ee6c33744a580e
Lucas Bonato <lucas.perez.bonato@gmail.com>
2024-03-27 19:05:02 -0300

    [TEST] Teste unitário para testar o código quando procurar um técnico.

--- src/test/java/com/sev/oeste/TecnicoControllerTest/GetTecnicoQueryHandlerTest.java
+++ src/test/java/com/sev/oeste/TecnicoControllerTest/GetTecnicoQueryHandlerTest.java
@@ -1,5 +1,6 @@
 package com.sev.oeste.TecnicoControllerTest;
 
+import com.sev.oeste.Exception.Tecnico.TecnicoNotFoundException;
 import com.sev.oeste.OesteApplication;
 import com.sev.oeste.Repository.TecnicoRepository;
 import com.sev.oeste.Tecnico.Models.Especialidade;
@@ -18,6 +19,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest(classes = OesteApplication.class)
@@ -43,7 +45,6 @@ public class GetTecnicoQueryHandlerTest {
         tecnico.setNome("Ana");
         tecnico.setSobrenome("Julia");
         tecnico.setTelefoneCelular("11938517043");
-        tecnico.setSituacao(Situacao.ATIVO);
         tecnico.setEspecialidades(especialidades);
 
         when(tecnicoRepository.findById(tecnico.getId())).thenReturn(Optional.of(tecnico));
@@ -51,6 +52,55 @@ public class GetTecnicoQueryHandlerTest {
         ResponseEntity<Tecnico> response = getTecnicoQueryHandler.execute(tecnico.getId());
         assertEquals(tecnico, response.getBody());
         assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    public void getTecnicoQueryHandler_validId_returnsOKStatusCode(){
+        List<Especialidade> especialidades = new ArrayList<>();
+
+        Especialidade esp = new Especialidade();
+        esp.setId(1);
+        esp.setConhecimento("Outros");
+
+        especialidades.add(esp);
+
+        Tecnico tecnico = new Tecnico();
+        tecnico.setId(1);
+        tecnico.setNome("Ana");
+        tecnico.setSobrenome("Julia");
+        tecnico.setTelefoneCelular("11938517043");
+        tecnico.setEspecialidades(especialidades);
+
+        when(tecnicoRepository.findById(tecnico.getId())).thenReturn(Optional.of(tecnico));
+
+        ResponseEntity<Tecnico> response = getTecnicoQueryHandler.execute(1);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    public void getTecnicoQueryHandler_invalidId_returns404AndException(){
+        List<Especialidade> especialidades = new ArrayList<>();
+
+        Especialidade esp = new Especialidade();
+        esp.setId(1);
+        esp.setConhecimento("Outros");
+
+        especialidades.add(esp);
+
+        Tecnico tecnico = new Tecnico();
+        tecnico.setId(1);
+        tecnico.setNome("Ana");
+        tecnico.setSobrenome("Julia");
+        tecnico.setTelefoneCelular("11938517043");
+        tecnico.setEspecialidades(especialidades);
+
+        //when(tecnicoRepository.findById(tecnico.getId())).thenReturn(Optional.of(tecnico));
+
+        TecnicoNotFoundException exception = assertThrows(
+                TecnicoNotFoundException.class,
+                () -> getTecnicoQueryHandler.execute(tecnico.getId())
+        );
+        assertEquals(HttpStatus.NOT_FOUND, exception.getStatus());
     }
 
 }
