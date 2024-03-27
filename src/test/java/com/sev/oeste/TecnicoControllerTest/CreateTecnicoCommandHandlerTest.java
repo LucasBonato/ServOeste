e56a82454dbcff78e645acad3bc08f45243dcc8c
Lucas Bonato <lucas.perez.bonato@gmail.com>
2024-03-27 19:53:26 -0300

    [BUILD] Abstraindo Ãmétodo para crianção do TecnicoDTO para evitar grande repetição de Ccódigos.

--- src/test/java/com/sev/oeste/TecnicoControllerTest/CreateTecnicoCommandHandlerTest.java
+++ src/test/java/com/sev/oeste/TecnicoControllerTest/CreateTecnicoCommandHandlerTest.java
@@ -29,28 +29,39 @@ public class CreateTecnicoCommandHandlerTest {
 
     @Test
     public void createTecnico_validTecnico_returnsCREATED(){
-        TecnicoDTO tecnicoDTO = new TecnicoDTO();
-        tecnicoDTO.setNome("Bla");
-        tecnicoDTO.setSobrenome("Bla");
-        tecnicoDTO.setTelefoneCelular("11976258312");
-        tecnicoDTO.setTelefoneFixo("");
         List<Integer> ids = new ArrayList<>();
+        List<String> conhecimentos = new ArrayList<>();
         ids.add(1);
+        conhecimentos.add("Outros");
+        ids.add(2);
+        conhecimentos.add("Geladeira");
+        TecnicoDTO tecnicoDTO = getTecnicoDTO("Bla", "Bla", "11976258312", "", ids, conhecimentos);
+
+        ResponseEntity response = createTecnico.execute(tecnicoDTO);
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+    }
+
+
+    private TecnicoDTO getTecnicoDTO(String nome, String sobrenome, String telefoneC, String telefoneF, List<Integer> ids, List<String> conhecimentos) {
+        TecnicoDTO tecnicoDTO = new TecnicoDTO();
+        tecnicoDTO.setNome(nome);
+        tecnicoDTO.setSobrenome(sobrenome);
+        tecnicoDTO.setTelefoneCelular(telefoneC);
+        tecnicoDTO.setTelefoneFixo(telefoneF);
         tecnicoDTO.setEspecialidades_Ids(ids);
 
-        Especialidade especialidade = new Especialidade();
-        especialidade.setId(3);
-        especialidade.setConhecimento("Outros");
-
         List<Especialidade> especialidades = new ArrayList<>();
-        especialidades.add(especialidade);
+        Especialidade especialidade = new Especialidade();
+        for(int i = 0; i < ids.size(); i++){
+            especialidade.setId(ids.get(i));
+            especialidade.setConhecimento(conhecimentos.get(i));
+            especialidades.add(especialidade);
+        }
 
         for(int i = 0; i < tecnicoDTO.getEspecialidades_Ids().size(); i++){
             when(especialidadeRepository.findById(tecnicoDTO.getEspecialidades_Ids().get(i))).thenReturn(Optional.of(especialidades.get(i)));
         }
-
-        ResponseEntity response = createTecnico.execute(tecnicoDTO);
-        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        return tecnicoDTO;
     }
 
 
