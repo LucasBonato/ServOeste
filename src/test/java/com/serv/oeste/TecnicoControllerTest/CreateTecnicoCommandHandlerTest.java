fbc7973668f362cf5f0e41d50c71af00d184cba9
Lucas Bonato <lucas.perez.bonato@gmail.com>
2024-03-29 09:58:31 -0300

    [REFACTOR] Evitando repetição de código colocando em uma classe base de testes.

--- src/test/java/com/serv/oeste/TecnicoControllerTest/CreateTecnicoCommandHandlerTest.java
+++ src/test/java/com/serv/oeste/TecnicoControllerTest/CreateTecnicoCommandHandlerTest.java
@@ -26,11 +26,8 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest(classes = OesteApplication.class)
-public class CreateTecnicoCommandHandlerTest {
+public class CreateTecnicoCommandHandlerTest extends BaseTest{
     @InjectMocks private CreateTecnicoCommandHandler createTecnico;
-
-    @Mock private TecnicoRepository tecnicoRepository;
-    @Mock private EspecialidadeRepository especialidadeRepository;
 
     @Test
     public void createTecnico_validTecnico_returnsCREATED(){
@@ -97,47 +94,5 @@ public class CreateTecnicoCommandHandlerTest {
                 () -> createTecnico.execute(tecnicoDTO)
         );
         Assertions.assertEquals("Especialidade não encontrada!", exception.getExceptionResponse().getMessage());
-    }
-
-    private TecnicoDTO getTecnicoDTO(String nome, String sobrenome, String telefoneC, String telefoneF, List<Integer> ids, List<String> conhecimentos) {
-        TecnicoDTO tecnicoDTO = new TecnicoDTO();
-        tecnicoDTO.setNome(nome);
-        tecnicoDTO.setSobrenome(sobrenome);
-        tecnicoDTO.setTelefoneCelular(telefoneC);
-        tecnicoDTO.setTelefoneFixo(telefoneF);
-        tecnicoDTO.setEspecialidades_Ids(ids);
-
-        List<Especialidade> especialidades = new ArrayList<>();
-        Especialidade especialidade = new Especialidade();
-        for(int i = 0; i < ids.size(); i++){
-            especialidade.setId(ids.get(i));
-            especialidade.setConhecimento(conhecimentos.get(i));
-            especialidades.add(especialidade);
-        }
-
-        for(int i = 0; i < tecnicoDTO.getEspecialidades_Ids().size(); i++){
-            when(especialidadeRepository.findById(tecnicoDTO.getEspecialidades_Ids().get(i))).thenReturn(Optional.of(especialidades.get(i)));
-        }
-        return tecnicoDTO;
-    }
-    private TecnicoDTO getTecnicoDTO(String nome, String sobrenome, String telefoneC, String telefoneF, List<Integer> ids) {
-        TecnicoDTO tecnicoDTO = new TecnicoDTO();
-        tecnicoDTO.setNome(nome);
-        tecnicoDTO.setSobrenome(sobrenome);
-        tecnicoDTO.setTelefoneCelular(telefoneC);
-        tecnicoDTO.setTelefoneFixo(telefoneF);
-        tecnicoDTO.setEspecialidades_Ids(ids);
-
-        List<Especialidade> especialidades = new ArrayList<>();
-        Especialidade especialidade = new Especialidade();
-        for(int i = 0; i < ids.size(); i++){
-            especialidade.setId(ids.get(i));
-            especialidades.add(especialidade);
-        }
-
-        for(int i = 0; i < tecnicoDTO.getEspecialidades_Ids().size(); i++){
-            when(especialidadeRepository.findById(tecnicoDTO.getEspecialidades_Ids().get(i))).thenReturn(Optional.empty());
-        }
-        return tecnicoDTO;
     }
 }
--- src/test/java/com/serv/oeste/TecnicoControllerTest/UpdateTecnicoCommandHandlerTest.java
+++ src/test/java/com/serv/oeste/TecnicoControllerTest/UpdateTecnicoCommandHandlerTest.java
@@ -7,8 +7,6 @@ import com.serv.oeste.Repository.TecnicoRepository;
 import com.serv.oeste.Tecnico.Command.CommandHandler.Models.UpdateTecnicoCommand;
 import com.serv.oeste.Tecnico.Command.CommandHandler.UpdateTecnicoCommandHandler;
 import com.serv.oeste.Tecnico.Models.DTOs.TecnicoDTO;
-import com.serv.oeste.Tecnico.Models.Tecnico;
-import com.serv.oeste.Tecnico.Models.Especialidade;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -25,11 +23,8 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest(classes = OesteApplication.class)
-public class UpdateTecnicoCommandHandlerTest {
+public class UpdateTecnicoCommandHandlerTest extends BaseTest {
     @InjectMocks private UpdateTecnicoCommandHandler updateTecnico;
-
-    @Mock private TecnicoRepository tecnicoRepository;
-    @Mock private EspecialidadeRepository especialidadeRepository;
 
     private Integer id = 1;
     private List<Integer> ids = new ArrayList<>();
@@ -61,34 +56,6 @@ public class UpdateTecnicoCommandHandlerTest {
 
         assertEquals(HttpStatus.NOT_FOUND, exception.getStatus());
         assertEquals("Técnico não encontrado!", exception.getExceptionResponse().getMessage());
-    }
-
-    private TecnicoDTO getTecnicoDTO(Integer id, String nome, String sobrenome, String telefoneC, String telefoneF, String situaco, List<Integer> ids, List<String> conhecimentos) {
-        TecnicoDTO tecnicoDTO = new TecnicoDTO();
-        tecnicoDTO.setNome(nome);
-        tecnicoDTO.setSobrenome(sobrenome);
-        tecnicoDTO.setTelefoneCelular(telefoneC);
-        tecnicoDTO.setTelefoneFixo(telefoneF);
-        tecnicoDTO.setEspecialidades_Ids(ids);
-        tecnicoDTO.setSituacao(situaco);
-
-        List<Especialidade> especialidades = new ArrayList<>();
-        Especialidade especialidade = new Especialidade();
-        for(int i = 0; i < ids.size(); i++){
-            especialidade.setId(ids.get(i));
-            especialidade.setConhecimento(conhecimentos.get(i));
-            especialidades.add(especialidade);
-        }
-
-        for(int i = 0; i < tecnicoDTO.getEspecialidades_Ids().size(); i++){
-            when(especialidadeRepository.findById(tecnicoDTO.getEspecialidades_Ids().get(i))).thenReturn(Optional.of(especialidades.get(i)));
-        }
-
-        when(tecnicoRepository.findById(id)).thenReturn(Optional.of(new Tecnico(tecnicoDTO)));
-        for(int i = 0; i < ids.size(); i++){
-            when(especialidadeRepository.findById(ids.get(i))).thenReturn(Optional.of(new Especialidade(ids.get(i), conhecimentos.get(i))));
-        }
-        return tecnicoDTO;
     }
 
 }
