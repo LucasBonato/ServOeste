66dc27c881f33b5c10d4a2a75b92f4a03b784d53
Lucas Bonato <lucas.perez.bonato@gmail.com>
2024-04-04 21:22:48 -0300

    [BUILD] Criação da query para conseguir pegar as especialidades requeridas quando se procura o Tecnico por nome/sobrenome.

--- src/main/java/com/serv/oeste/Models/DTOs/TecnicoDTO.java
+++ src/main/java/com/serv/oeste/Models/DTOs/TecnicoDTO.java
@@ -1,10 +1,12 @@
 package com.serv.oeste.Models.DTOs;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@NoArgsConstructor
 public class TecnicoDTO {
     private String nome;
     private String sobrenome;
@@ -12,4 +14,8 @@ public class TecnicoDTO {
     private String telefoneCelular;
     private String situacao;
     private List<Integer> especialidades_Ids;
+
+    public TecnicoDTO(List<Integer> especialidades_Ids){
+        this.especialidades_Ids = especialidades_Ids;
+    }
 }
--- src/main/java/com/serv/oeste/Repository/TecnicoRepository.java
+++ src/main/java/com/serv/oeste/Repository/TecnicoRepository.java
@@ -1,5 +1,6 @@
 package com.serv.oeste.Repository;
 
+import com.serv.oeste.Models.DTOs.TecnicoDTO;
 import com.serv.oeste.Models.Situacao;
 import com.serv.oeste.Models.Tecnico;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -19,4 +20,12 @@ public interface TecnicoRepository extends JpaRepository<Tecnico, Integer> {
             LIKE %:nomeOuSobrenome%
         """)
     List<Tecnico> findByNomeOuSobrenome(@Param("nomeOuSobrenome") String nomeOuSobrenome);
+
+    @Query(value = """
+            SELECT te.id_especialidade FROM Tecnico t
+            INNER JOIN Tecnico_Especialidade te ON t.Id = te.Id_Tecnico
+            INNER JOIN Especialidade e ON e.Id = te.Id_Especialidade
+            WHERE t.Id = :Id_Especialidade
+        """, nativeQuery = true)
+    List<Integer> findByIdEspecialidade(@Param("Id_Especialidade") Integer id_Especialidade);
 }
