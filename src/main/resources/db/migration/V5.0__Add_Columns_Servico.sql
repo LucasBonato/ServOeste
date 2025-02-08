a848b667f77d5aa117833c5d243610869ae3af23
Lucas Bonato <lucas.perez.bonato@gmail.com>
2025-02-08 15:59:46 -0300

    [FIX] Table not being find because of upper case

--- src/main/resources/db/migration/V5.0__Add_Columns_Servico.sql
+++ src/main/resources/db/migration/V5.0__Add_Columns_Servico.sql
@@ -1,4 +1,4 @@
-ALTER TABLE Servico ADD forma_pagamento ENUM('DEBITO', 'CREDITO', 'PIX', 'BOLETO') DEFAULT NULL;
-ALTER TABLE Servico ADD valor_pecas double DEFAULT NULL;
-ALTER TABLE Servico ADD valor_comissao double DEFAULT NULL;
-ALTER TABLE Servico ADD data_pagamento_comissao datetime(6) DEFAULT NULL;
+ALTER TABLE servico ADD forma_pagamento ENUM('DEBITO', 'CREDITO', 'PIX', 'BOLETO') DEFAULT NULL;
+ALTER TABLE servico ADD valor_pecas double DEFAULT NULL;
+ALTER TABLE servico ADD valor_comissao double DEFAULT NULL;
+ALTER TABLE servico ADD data_pagamento_comissao datetime(6) DEFAULT NULL;
