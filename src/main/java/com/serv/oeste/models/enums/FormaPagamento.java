d42aac5f9294d601f0752b869836a67fe10acac2
Lucas Bonato <lucas.perez.bonato@gmail.com>
2025-02-08 22:26:36 -0300

    [FIX] Add money as a type of payment

--- src/main/java/com/serv/oeste/models/enums/FormaPagamento.java
+++ src/main/java/com/serv/oeste/models/enums/FormaPagamento.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum FormaPagamento {
     DEBITO("Débito"),
+    DINHEIRO("Dinheiro"),
     CREDITO("Crédito"),
     PIX("PIX"),
     BOLETO("Boleto"),
--- src/main/resources/db/migration/V5.0__Add_Columns_Servico.sql
+++ src/main/resources/db/migration/V5.0__Add_Columns_Servico.sql
@@ -1,4 +1,4 @@
-ALTER TABLE servico ADD forma_pagamento ENUM('DEBITO', 'CREDITO', 'PIX', 'BOLETO') DEFAULT NULL;
+ALTER TABLE servico ADD forma_pagamento ENUM('DEBITO', 'DINHEIRO', 'CREDITO', 'PIX', 'BOLETO') DEFAULT NULL;
 ALTER TABLE servico ADD valor_pecas double DEFAULT NULL;
 ALTER TABLE servico ADD valor_comissao double DEFAULT NULL;
 ALTER TABLE servico ADD data_pagamento_comissao datetime(6) DEFAULT NULL;
