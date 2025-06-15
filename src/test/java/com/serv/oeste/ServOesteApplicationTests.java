5e00da2ece4002d7ec003721d635193d42d3b877
LucasBonato <lucas.perez.bonato@gmail.com>
2025-06-15 06:13:33 -0300

    [TEST] Create an Memory Database H2 for tests

--- src/test/java/com/serv/oeste/ServOesteApplicationTests.java
+++ src/test/java/com/serv/oeste/ServOesteApplicationTests.java
@@ -2,8 +2,10 @@ package com.serv.oeste;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ServOesteApplicationTests {
 
 	@Test
