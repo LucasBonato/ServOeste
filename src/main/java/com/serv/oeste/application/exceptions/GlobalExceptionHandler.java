b6d8697190ebf2117ed5b98b4af49a48517fe750
LucasBonato <lucas.perez.bonato@gmail.com>
2025-07-17 16:05:10 -0300

    [FEAT] Initial test with Problem Details in Spring

--- src/main/java/com/serv/oeste/application/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/serv/oeste/application/exceptions/GlobalExceptionHandler.java
@@ -1,15 +1,28 @@
 package com.serv.oeste.application.exceptions;
 
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(BaseException.class)
-    public ResponseEntity<ExceptionResponse> tecnicoExceptionsHandler(BaseException exception){
-        return ResponseEntity
-                .status(exception.getStatus())
-                .body(exception.getExceptionResponse());
+    public ProblemDetail exceptionsHandler(BaseException exception){
+        ProblemDetail problemDetail = ProblemDetail
+                .forStatusAndDetail(
+                        exception.getStatus(),
+                        exception.getExceptionResponse().getMessage()
+                );
+
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("error", exception.getExceptionResponse());
+
+        problemDetail.setProperties(properties);
+
+        return problemDetail;
     }
 }
