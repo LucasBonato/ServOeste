14572a0915303571798adff3d981709140eb7b6f
LucasBonato <lucas.perez.bonato@gmail.com>
2025-10-12 03:19:25 -0300

    [REFACTOR] Remove HTTP related code from Application layer

--- src/main/java/com/serv/oeste/application/services/AddressService.java
+++ src/main/java/com/serv/oeste/application/services/AddressService.java
@@ -1,15 +1,14 @@
 package com.serv.oeste.application.services;
 
 import com.serv.oeste.application.dtos.reponses.EnderecoResponse;
+import com.serv.oeste.domain.entities.viacep.ViaCep;
 import com.serv.oeste.domain.enums.ErrorFields;
-import com.serv.oeste.domain.exceptions.external.RestTemplateException;
+import com.serv.oeste.domain.exceptions.address.AddressNotValidException;
 import com.serv.oeste.domain.exceptions.external.ExternalNetworkException;
 import com.serv.oeste.domain.exceptions.external.ExternalServerDownException;
-import com.serv.oeste.domain.entities.viacep.ViaCep;
-import com.serv.oeste.domain.exceptions.address.AddressNotValidException;
+import com.serv.oeste.domain.exceptions.external.RestTemplateException;
 import io.micrometer.common.util.StringUtils;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.*;
 
@@ -18,8 +17,8 @@ import org.springframework.web.client.*;
 public class AddressService {
     private final RestTemplate restTemplate;
     
-    public ResponseEntity<EnderecoResponse> getFields(String cep) {
-        return ResponseEntity.ok(getViaCepObject(cep));
+    public EnderecoResponse getFields(String cep) {
+        return getViaCepObject(cep);
     }
     
     protected EnderecoResponse getViaCepObject(String cep) {
