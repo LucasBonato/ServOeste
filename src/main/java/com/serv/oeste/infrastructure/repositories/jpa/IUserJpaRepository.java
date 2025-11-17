9f1f7c46f258edcd6cf6c4a0729d372de3bc3ed9
LucasBonato <lucas.perez.bonato@gmail.com>
2025-11-17 20:12:38 -0300

    [FIX] Pass DTO for the user final without the password hash

--- src/main/java/com/serv/oeste/infrastructure/repositories/jpa/IUserJpaRepository.java
+++ src/main/java/com/serv/oeste/infrastructure/repositories/jpa/IUserJpaRepository.java
@@ -2,19 +2,12 @@ package com.serv.oeste.infrastructure.repositories.jpa;
 
 import com.serv.oeste.infrastructure.entities.user.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-import java.util.UUID;
 
 @Repository
-public interface IUserJpaRepository extends JpaRepository<UserEntity, UUID> {
-    Optional<UserEntity> findById(Integer id);
+public interface IUserJpaRepository extends JpaRepository<UserEntity, Integer> {
     Optional<UserEntity> findByUsername(String username);
-
-    @Modifying
-    @Query("DELETE FROM UserEntity u WHERE u.username = :username")
     void deleteByUsername(String username);
 }
