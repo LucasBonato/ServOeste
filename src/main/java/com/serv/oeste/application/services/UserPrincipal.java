5d9c63ad5fcc7bc312d0245fff3b7f7e69838103
LucasBonato <lucas.perez.bonato@gmail.com>
2026-01-13 00:08:54 -0300

    [STYLE] Remove unnused imports

--- src/main/java/com/serv/oeste/application/services/UserPrincipal.java
+++ src/main/java/com/serv/oeste/application/services/UserPrincipal.java
@@ -8,13 +8,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.List;
 
-public class UserPrincipal implements UserDetails {
-    private final User user;
-
-    public UserPrincipal(User user) {
-        this.user = user;
-    }
-
+public record UserPrincipal(
+        User user
+) implements UserDetails {
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of(new SimpleGrantedAuthority(user.getRole().getRoleWithPrefix()));
--- src/main/java/com/serv/oeste/domain/exceptions/user/UserNotValidException.java
+++ src/main/java/com/serv/oeste/domain/exceptions/user/UserNotValidException.java
@@ -1,7 +1,6 @@
 package com.serv.oeste.domain.exceptions.user;
 
 import com.serv.oeste.domain.enums.ErrorFields;
-import com.serv.oeste.domain.exceptions.NotFoundException;
 import com.serv.oeste.domain.exceptions.NotValidException;
 
 public class UserNotValidException extends NotValidException {
