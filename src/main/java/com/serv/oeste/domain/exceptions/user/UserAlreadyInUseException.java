a84bce93e6ed4765b99336de5f49b451600de9d6
LucasBonato <lucas.perez.bonato@gmail.com>
2026-01-13 00:08:54 -0300

    [STYLE] Remove unnused imports

--- src/main/java/com/serv/oeste/domain/exceptions/user/UserAlreadyInUseException.java
+++ src/main/java/com/serv/oeste/domain/exceptions/user/UserAlreadyInUseException.java
@@ -1,8 +1,5 @@
 package com.serv.oeste.domain.exceptions.user;
 
-import com.serv.oeste.domain.enums.ErrorFields;
-import com.serv.oeste.domain.exceptions.NotFoundException;
-
 public class UserAlreadyInUseException extends UserNotValidException {
     public UserAlreadyInUseException() {
         super("Nome de usuário já está em uso");
